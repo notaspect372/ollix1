fa1eeb7d0370b39eb286667da9782a413c565bcf
notaspect372 <notaspect372@gmail.com>
2025-01-29 21:46:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -345,9 +345,9 @@ def scrape_data(brands, start_page, end_page):      # Configure Edge options
 
 
 # enter the brand urls here
-brands = ["https://floorscenter.com/collections/daltile"]
+brands = ["https://floorscenter.com/collections/ms-international"]
 
-start_page =61
-end_page = 70
+start_page =46
+end_page = 50
 
 scrape_data(brands, start_page, end_page)
